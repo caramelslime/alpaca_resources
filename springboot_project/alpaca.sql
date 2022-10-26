b5c6af86816484cc459a04553da6cd02ea149564
caramelslime <112607458+caramelslime@users.noreply.github.com>
2022-10-26 14:36:00 +0900

    Add files via upload

--- springboot_project/alpaca.sql
+++ springboot_project/alpaca.sql
@@ -1,125 +1,100 @@
-ï»¿CREATE TABLE `Untitled` (
-	`id`	varchar(40)	NOT NULL,
-	`password`	varchar(100)	NOT NULL,
-	`name`	varchar(20)	NOT NULL,
-	`nick_name`	varchar(40)	NOT NULL,
-	`call`	varchar(20)	NOT NULL,
-	`email`	varchar(50)	NOT NULL,
-	`address`	varchar(60)	NOT NULL,
-	`created_date`	date	NOT NULL,
-	`del`	char(1)	NOT NULL	DEFAULT n,
-	`authority`	char(1)	NULL	DEFAULT n,
-	`age`	varchar(10)	NOT NULL	DEFAULT 20ëŒ€,
-	`gender`	char(1)	NOT NULL	DEFAULT m
+--È¸¿ø
+CREATE TABLE member (
+	id	varchar(40)	NOT NULL primary key,
+	password	varchar(100)	NOT NULL,
+	name	varchar(20)	NOT NULL,
+	call	varchar(20)	NOT NULL,
+	email	varchar(50)	NOT NULL,
+	address	varchar(60)	NOT NULL,
+	created_date	date	NOT NULL,
+	del	char(1) DEFAULT 'n'	NOT NULL,
+	authority	char(1) DEFAULT 'n'	NOT NULL,
+	age	varchar(10) DEFAULT '20´ë'	NOT NULL,
+	gender	char(1) DEFAULT 'm'	NOT NULL	
 );
+--°æ¸Å ¹°Ç°
+CREATE TABLE auction_item (
+	auction_no	number(10)	NOT NULL primary key,
+	item_name	varchar(20)	NOT NULL,
+	item_contents	varchar(300)	NOT NULL,
+	item_img	varchar(200)	NOT NULL,
+	start_date	date	NOT NULL,
+	end_date	date	NOT NULL,
+	start_price	number(10)	NOT NULL,
+	min_bid	number(10)	NOT NULL,
+	view_cnt	number(10)	NOT NULL,
+	del	char(1) DEFAULT 'n'	NOT NULL,
+	delivery	varchar(10)	NULL,
+	reg_date	date	NOT NULL,
+	pay	char(1) default 'n' NOT NULL,
+	id	varchar(40)	NOT NULL references member(id),
+	tag_no	number(10)	NOT NULL
+);
+--°æ¸Å ÈÄ±â °Ô½ÃÆÇ
+CREATE TABLE review_board (
+	review_no	number(10)	NOT NULL primary key,
+	title	varchar(40)	NOT NULL,
+	contents	varchar(300)	NOT NULL,
+	review_img	varchar(200)	NOT NULL,
+	read_cnt	number(10)	NOT NULL,
+	del	char(1) DEFAULT 'n'	NOT NULL,
+	reg_date	date	NOT NULL,
+	rating	number(5)	NOT NULL,
+	id	varchar(40)	NOT NULL references member(id),
+	auction_no	number(10)	NOT NULL references auction_item(auction_no)
+);
+--°ü½É¹°Ç°
+CREATE TABLE interest (
+	interest_no	number(10)	NOT NULL primary key,
+	id	varchar(40)	NOT NULL references member(id),
+	auction_no	number(10)	NOT NULL references auction_item(auction_no)
+);
+--ÀÔÂû
+CREATE TABLE bidding (
+	bid_no	number(10)	NOT NULL primary key,
+	bid_price	number(10)	NOT NULL,
+	bid_date	number(10)	NOT NULL,
+	id	varchar(40)	NOT NULL references member(id),
+	auction_no	number(10)	NOT NULL references auction_item(auction_no)
+);
+--°ü½ÉÅÂ±×
+CREATE TABLE interest_tag (
+	it_tag_no	number(10)	NOT NULL primary key,
+	id	varchar(40)	NOT NULL references member(id),
+	tag_no number(10)	NOT NULL
+);
+--ÅÂ±×¸ñ·Ï
+CREATE TABLE tag (
+	tag_no	number(10)	NOT NULL primary key,
+	tag_name	varchar(20)	NOT NULL
+);
+--ÈÄ±â°Ô½ÃÆÇ ´ñ±Û
+CREATE TABLE review_reply (
+	re_reply_no	number(10)	NOT NULL primary key,
+	reply_contents	varchar(100)	NOT NULL,
+	del	char(1) 	DEFAULT 'n'	NOT NULL,
+	reg_date	date	NOT NULL,
+	up	char(1)	DEFAULT 'n'	NOT NULL,--¼öÁ¤¿©ºÎ
+	id	varchar(40)	NOT NULL references member(id),
+	review_no	number(10)	NOT NULL
+);
+--¹®ÀÇ °Ô½ÃÆÇ
+CREATE TABLE qna_board (
+	qna_no	number(10)	NOT NULL primary key,
+	qna_title	varchar(40)	NULL,
+	qna_contents	varchar(300)	NULL,
+	del	char(1)		DEFAULT 'n' not NULL,
+	reg_date	date	NULL,
+	id	varchar(40)	NOT NULL references member(id)
+);
+drop table member cascade constraint;
+drop table auction_item cascade constraint;
+drop table review_board cascade constraint;
+drop table interest cascade constraint;
+drop table bidding cascade constraint;
+drop table interest_tag cascade constraint;
+drop table tag cascade constraint;
+drop table review_reply cascade constraint;
+drop table qna_board cascade constraint;
 
-CREATE TABLE `auction_item` (
-	`auction_no`	number(10)	NOT NULL,
-	`item_name`	varchar(20)	NOT NULL,
-	`item_contents`	varchar(300)	NOT NULL,
-	`item_img`	varchar(200)	NOT NULL,
-	`start_date`	date	NOT NULL,
-	`end_date`	date	NOT NULL,
-	`start_price`	number(10)	NOT NULL,
-	`min_bid`	number(10)	NOT NULL,
-	`view_cnt`	number(10)	NOT NULL,
-	`del`	char(1)	NOT NULL,
-	`delivery`	varchar(10)	NULL,
-	`reg_date`	date	NOT NULL,
-	`id`	varchar(40)	NOT NULL,
-	`tag_no`	number(10)	NOT NULL
-);
-
-CREATE TABLE `review_board` (
-	`review_no`	number(10)	NOT NULL,
-	`title`	varchar(40)	NOT NULL,
-	`contents`	varchar(300)	NOT NULL,
-	`review_img`	varchar(200)	NOT NULL,
-	`read_cnt`	number(10)	NOT NULL,
-	`del`	char(1)	NOT NULL,
-	`reg_date`	date	NOT NULL,
-	`rating`	number(5)	NOT NULL,
-	`id`	varchar(40)	NOT NULL,
-	`auction_no`	number(10)	NOT NULL
-);
-
-CREATE TABLE `interest` (
-	`interest_no`	number(10)	NOT NULL,
-	`id`	varchar(40)	NOT NULL,
-	`auction_no`	number(10)	NOT NULL
-);
-
-CREATE TABLE `bidding` (
-	`bid_no`	number(10)	NOT NULL,
-	`bid_price`	number(10)	NOT NULL,
-	`bid_date`	number(10)	NOT NULL,
-	`id`	varchar(40)	NOT NULL,
-	`auction_no`	number(10)	NOT NULL
-);
-
-CREATE TABLE `interest_tag` (
-	`it_tag_no`	number(10)	NOT NULL,
-	`id`	varchar(40)	NOT NULL,
-	`tag_no`	number(10)	NOT NULL
-);
-
-CREATE TABLE `tag` (
-	`tag_no`	number(10)	NOT NULL,
-	`tag_name`	varchar(20)	NOT NULL
-);
-
-CREATE TABLE `review_reply` (
-	`re_reply_no`	number(10)	NOT NULL,
-	`reply_contents`	varchar(100)	NOT NULL,
-	`del`	char(1)	NOT NULL	DEFAULT n,
-	`reg_date`	date	NOT NULL,
-	`up`	char(1)	NOT NULL	DEFAULT n,
-	`id`	varchar(40)	NOT NULL,
-	`review_no`	number(10)	NOT NULL
-);
-
-CREATE TABLE `qna_board` (
-	`qna_no`	number(10)	NOT NULL,
-	`qna_title`	varchar(40)	NULL,
-	`qna_contents`	varchar(300)	NULL,
-	`del`	char(1)	NULL,
-	`reg_date`	date	NULL,
-	`id`	varchar(40)	NOT NULL
-);
-
-ALTER TABLE `Untitled` ADD CONSTRAINT `PK_UNTITLED` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `auction_item` ADD CONSTRAINT `PK_AUCTION_ITEM` PRIMARY KEY (
-	`auction_no`
-);
-
-ALTER TABLE `review_board` ADD CONSTRAINT `PK_REVIEW_BOARD` PRIMARY KEY (
-	`review_no`
-);
-
-ALTER TABLE `interest` ADD CONSTRAINT `PK_INTEREST` PRIMARY KEY (
-	`interest_no`
-);
-
-ALTER TABLE `bidding` ADD CONSTRAINT `PK_BIDDING` PRIMARY KEY (
-	`bid_no`
-);
-
-ALTER TABLE `interest_tag` ADD CONSTRAINT `PK_INTEREST_TAG` PRIMARY KEY (
-	`it_tag_no`
-);
-
-ALTER TABLE `tag` ADD CONSTRAINT `PK_TAG` PRIMARY KEY (
-	`tag_no`
-);
-
-ALTER TABLE `review_reply` ADD CONSTRAINT `PK_REVIEW_REPLY` PRIMARY KEY (
-	`re_reply_no`
-);
-
-ALTER TABLE `qna_board` ADD CONSTRAINT `PK_QNA_BOARD` PRIMARY KEY (
-	`qna_no`
-);
 
